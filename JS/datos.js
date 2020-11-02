94a71f06f2f9aa325097744a25b5159c388f0a91
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2020-11-01 23:04:45 -0600

    sf

--- JS/datos.js
+++ JS/datos.js
@@ -3,29 +3,7 @@ var getDatos= function(){
     var nombre= document.getElementById("nombre").value;
     var Telefono = document.getElementById("Telefono").value;
      var correo = document.getElementById("correo").value;
-    if(nombre==""){
-        document.getElementById("nombre").focus();
 
-    }else{
-        if(edad==""){
-            document.getElementById("edad").focus();
-        }else{
-            if (/^[a-zA-Z ]+$/.test(nombre))
-    {
-        console.log("Nombre: " + nombre);
-
-    }else{
-        alert("Nombre incorrecto, solo se permiten letras");
-    }
-            if( isNaN(Telefono) ) {
-    alert("Edad Incorrecta. La edad solo debe contener numeros");
-   }else{
-    console.log("Telefono: " + Telefono);
-}
-             validarEmail(correo);
-        }
-    }
-     }
   function validarEmail(valor) {
     if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
      alert("La dirección de email " + valor + " es correcta!.");
