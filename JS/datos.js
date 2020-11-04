ab098cfe62e49e6c10c2bcf00c609de880921ca5
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2020-11-03 23:20:45 -0600

    datos

--- JS/datos.js
+++ JS/datos.js
@@ -1,8 +1,5 @@
 var getDatos= function(){
-
-    var nombre= document.getElementById("nombre").value;
-    var Telefono = document.getElementById("Telefono").value;
-     var correo = document.getElementById("correo").value;
+ var correo = document.getElementById("correo").value;
 
   function validarEmail(valor) {
     if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
