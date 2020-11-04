0f1f59c4b3b9c5aca34e613f241a4f551ec6e077
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2020-11-03 23:30:42 -0600

    validar

--- JS/datos.js
+++ JS/datos.js
@@ -1,5 +1,75 @@
 var getDatos= function(){
  var correo = document.getElementById("correo").value;
+var nombre = document.getElementById("nombre").value;
+var Apellidos = document.getElementById("Apellidos").value;
+var Telefono = document.getElementById("Telefono").value;
+     if(correo==""){
+        document.getElementById("correo").focus();
+
+    }else{
+        if(nombre==""){
+            document.getElementById("nombre").focus();
+        }else{
+
+    if(Apellidos==""){
+            document.getElementById("Apellidos").focus();
+    }else{
+
+    if(Telefono==""){
+            document.getElementById("Telefono").focus();
+            }else{
+     if (/^[a-zA-Z ]+$/.test(nombre))
+    {
+        console.log("Nombre: " + nombre);
+
+    }else{
+        alert("Nombre incorrecto, solo se permiten letras");
+    }
+
+
+             if (/^[a-zA-Z ]+$/.test(Apellidos))
+    {
+        console.log("Apellidos: " + Apellidos);
+
+    }else{
+        alert("Apellidos incorrecto, solo se permiten letras");
+    }
+
+
+
+
+
+
+            if( Telefono(edad) ) {
+    alert("Edad Incorrecta. La edad solo debe contener numeros");
+   }else{
+    console.log("Telefono: " + Telefono);
+}
+             validarEmail(correo);
+        }
+    }
+     }
+        }
+
+
+
+
+
+
+
+
+    validarEmail(correo);
+
+
+
+
+
+
+
+
+
+
+
 
   function validarEmail(valor) {
     if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
